6fa786020e5ddc06d8699f21c1e99c3d038f2503
Oeronteros <ruslan.zokirov.2001@gmail.com>
2025-12-26 10:30:29 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NODE_ENV: process.env.NODE_ENV,
-  },
 };
 
 export default nextConfig;
