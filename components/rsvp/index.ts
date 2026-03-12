841f0de760288d924ce7cfe2a5d601188b52aebb
Oeronteros <ruslan.zokirov.2001@gmail.com>
2026-03-12 09:02:37 +0300

    ав

--- components/rsvp/index.ts
+++
@@ -1,5 +0,0 @@
-export { RsvpButton } from './RsvpButton';
-export { RsvpSummaryDisplay } from './RsvpSummary';
-export { CalendarView } from './CalendarView';
-export { useRsvps, useRsvpSummary, useUpsertRsvp, useUpdateRsvp, useDeleteRsvp } from '@/lib/rsvp';
-export type { RsvpStatus } from '@/lib/schemas/rsvp';
